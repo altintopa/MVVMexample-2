ed640508762d5fe77c26e1deb7e101546d4235e0
altintopa <83283552+altintopa@users.noreply.github.com>
2022-05-17 16:31:05 +0300

    littleFixes

--- MVVMexample-2/ViewController.swift
+++ MVVMexample-2/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tblView.register(UINib(nibName: "tblViewCell", bundle: nil), forCellReuseIdentifier: "cellID")
-        fillArray()()
+        fillArray()
     }
     
     func fillArray(){
